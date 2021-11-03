a9aad714d49f9e3af376968561f1073820c07e18
Mr. Smith <csmith@ytech.edu>
2021-11-03 10:43:12 -0400

    Create 01_assignment.py

---
+++ 01_assignment.py
@@ -0,0 +1,46 @@
+
+age = 16
+grade = 98.6
+name = "Jeff"
+favoriteNumber = "69"
+
+# Boolean: True or False
+# True is any value that is not False or empty
+isSmart = True
+
+#You can output to the console by using 'print()'
+print(age)
+print(grade)
+print(name)
+print(favoriteNumber)
+print(isSmart)
+
+#You can concatenate simliar values together
+print("My name " + name)
+#You can use functions to convert datatypes
+print("and my age is " + str(age))
+#Don't forget! If you want to convert a value permanently
+#You must assign the converted value to a variable
+age = str(age)
+#You can convert back and forth with int(), str(), bool(), and float()
+print(int(age))
+print(float(age))
+print(bool(age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#EOF
+
